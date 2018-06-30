f7c38631eb372581eff660d1eacb236bfa291c95
Thauvin Alexandre <alexandre.thauvin@outlook.com>
2018-07-01 01:52:48 +0200

    bluetooth service implemented

--- app/src/main/java/alexandre/thauvin/smarttoolbox/MainActivity.java
+++ app/src/main/java/alexandre/thauvin/smarttoolbox/MainActivity.java
@@ -1,13 +1,19 @@
 package alexandre.thauvin.smarttoolbox;
 
+import android.bluetooth.BluetoothAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    BluetoothAdapter bluetoothAdapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        bluetoothAdapter.disable();
     }
 }
