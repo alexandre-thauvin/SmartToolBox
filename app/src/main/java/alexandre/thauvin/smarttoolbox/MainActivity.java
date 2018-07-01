25c1993463fbc4dd0397c8a2e4223b7b6c72a268
Thauvin Alexandre <alexandre.thauvin@outlook.com>
2018-07-01 19:09:29 +0200

    unzip icon + delete .zip, set textview with the pickup

--- app/src/main/java/alexandre/thauvin/smarttoolbox/MainActivity.java
+++ app/src/main/java/alexandre/thauvin/smarttoolbox/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.TimePicker;
 import android.support.v4.app.DialogFragment;
 
@@ -24,8 +25,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, ShutDownService.class);
-        startService(intent);
+        /*Intent intent = new Intent(this, ShutDownService.class);
+        startService(intent);*/
+
 
     }
 
@@ -51,7 +53,11 @@ public class MainActivity extends AppCompatActivity {
         }
 
         public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-            // Do something with the time chosen by the user
+            TextView textView = getActivity().findViewById(R.id.hour_bluetooth);
+            StringBuilder formattedTime = new StringBuilder().append(hourOfDay)
+                    .append(":")
+                    .append(minute < 10 ? "0" + minute : minute);
+            textView.setText(formattedTime);
 
         }
     }
