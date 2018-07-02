5128be7154b735354ffe8537335c0304329170b8
Thauvin Alexandre <alexandre.thauvin@outlook.com>
2018-07-02 20:22:52 +0200

    TimePicker Widget + done miss action

--- app/src/main/java/alexandre/thauvin/smarttoolbox/MainActivity.java
+++ app/src/main/java/alexandre/thauvin/smarttoolbox/MainActivity.java
@@ -1,43 +1,49 @@
 package alexandre.thauvin.smarttoolbox;
 
-import android.app.Dialog;
-import android.app.TimePickerDialog;
-import android.bluetooth.BluetoothAdapter;
+
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.format.DateFormat;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.TimePicker;
-import android.support.v4.app.DialogFragment;
-
-
-import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
 
 
     private int res = 0;
+    private TimePicker tp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*Intent intent = new Intent(this, ShutDownService.class);
-        startService(intent);*/
 
 
+        tp = findViewById(R.id.timePicker);
+        tp.setOnTimeChangedListener(timeChangedListener);
+        tp.setVisibility(View.GONE);
+        tp.setIs24HourView(true);
     }
 
-    public void toTest(View view)
-    {
-        Intent intent = new Intent(this, TestActivity.class);
-        startActivity(intent);
-    }
+    private TimePicker.OnTimeChangedListener timeChangedListener =
+            new TimePicker.OnTimeChangedListener(){
+                @Override
+                public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+                    TextView textView = findViewById(R.id.hour_bluetooth);
+                    StringBuilder formattedTime = new StringBuilder().append(hourOfDay)
+                            .append(":")
+                            .append(minute < 10 ? "0" + minute : minute);
+                    textView.setText(formattedTime);
+                    res = hourOfDay + minute;
 
-    public static class TimePickerFragment extends DialogFragment
+                }
+            };
+
+
+   /* public static class TimePickerFragment extends DialogFragment
             implements TimePickerDialog.OnTimeSetListener {
 
         @Override
@@ -53,26 +59,30 @@ public class MainActivity extends AppCompatActivity {
         }
 
         public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-            TextView textView = getActivity().findViewById(R.id.hour_bluetooth);
-            StringBuilder formattedTime = new StringBuilder().append(hourOfDay)
-                    .append(":")
-                    .append(minute < 10 ? "0" + minute : minute);
-            textView.setText(formattedTime);
+
 
         }
-    }
+    }*/
 
     public void showTimePickerDialog(View v) {
-        DialogFragment newFragment = new TimePickerFragment();
-        newFragment.show(getSupportFragmentManager(), "timePicker");
+        if (tp.getVisibility() == View.GONE)
+         tp.setVisibility(View.VISIBLE);
+        else
+            tp.setVisibility(View.GONE);
+
     }
 
    public void checkActions(View view)
    {
+       CheckBox cb = findViewById(R.id.checkbox_bluetooth);
+
+       if (cb.isChecked()) {
+           Intent intent = new Intent(this, ShutDownService.class);
+           Bundle b = new Bundle();
+           b.putInt("time", res); //Your id
+           intent.putExtras(b); //Put your id to your next Intent
+           startService(intent);
+       }
 
    }
-
-    public void setRes(int res) {
-        this.res = res;
-    }
 }
--- app/src/main/java/alexandre/thauvin/smarttoolbox/ShutDownService.java
+++ app/src/main/java/alexandre/thauvin/smarttoolbox/ShutDownService.java
@@ -3,6 +3,7 @@ package alexandre.thauvin.smarttoolbox;
 import android.app.Service;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.IBinder;
@@ -10,15 +11,13 @@ import android.os.Looper;
 import android.os.Message;
 import android.os.Process;
 import android.widget.Toast;
-
 import java.util.Calendar;
-import java.util.Date;
 
 public class ShutDownService extends Service {
 
     private Looper mServiceLooper;
     private ServiceHandler mServiceHandler;
-    private long time;
+    private int time = 0;
 
     // Handler that receives messages from the thread
     private final class ServiceHandler extends Handler {
@@ -29,10 +28,10 @@ public class ShutDownService extends Service {
         public void handleMessage(Message msg) {
             // Normally we would do some work here, like download a file.
             // For our sample, we just sleep for 5 seconds.
-            int time = Calendar.getInstance().get(Calendar.HOUR_OF_DAY) + Calendar.getInstance().get(Calendar.MINUTE);
-            while (time <= 15+17)
+            int currentTime = Calendar.getInstance().get(Calendar.HOUR_OF_DAY) + Calendar.getInstance().get(Calendar.MINUTE);
+            while (currentTime != time)
             {
-                time = Calendar.getInstance().get(Calendar.HOUR_OF_DAY) + Calendar.getInstance().get(Calendar.MINUTE);
+                currentTime = Calendar.getInstance().get(Calendar.HOUR_OF_DAY) + Calendar.getInstance().get(Calendar.MINUTE);
             }
 
                 BluetoothAdapter bluetoothAdapter;
@@ -51,6 +50,7 @@ public class ShutDownService extends Service {
         // separate thread because the service normally runs in the process's
         // main thread, which we don't want to block. We also make it
         // background priority so CPU-intensive work doesn't disrupt our UI.
+
         HandlerThread thread = new HandlerThread("ServiceStartArguments",
                 Process.THREAD_PRIORITY_BACKGROUND);
         thread.start();
@@ -63,6 +63,9 @@ public class ShutDownService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show();
+        Bundle b = intent.getExtras();
+        if(b != null)
+            time = b.getInt("time");
 
         // For each start request, send a message to start a job and deliver the
         // start ID so we know which request we're stopping when we finish the job
